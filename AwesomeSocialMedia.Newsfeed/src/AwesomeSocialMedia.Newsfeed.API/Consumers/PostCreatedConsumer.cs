3fc08303bf52239117a2b56593b698d627356958
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-12-26 17:17:50 -0300

    Add project files.

---
+++ AwesomeSocialMedia.Newsfeed/src/AwesomeSocialMedia.Newsfeed.API/Consumers/PostCreatedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using AwesomeSocialMedia.Newsfeed.API.Core.Entities;
+using AwesomeSocialMedia.Newsfeed.API.Core.Repositories;
+using Microsoft.AspNetCore.Connections;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AwesomeSocialMedia.Newsfeed.API.Consumers
+{
+    public class PostCreatedConsumer
+    {
+       
+    }
+
+    public class PostCreatedEvent
+	{
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+		public DateTime PublishedAt { get; set; }
+		public User User { get; set; }
+	}
+}
+
